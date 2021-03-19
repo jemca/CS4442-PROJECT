749f0306d6d28f033e898f223ee366eacd0a528d
jemca <46153814+jemca@users.noreply.github.com>
2021-03-19 03:18:51 -0400

    working on git

---
+++ Assets/Flappy Bird Style/ML-Agents/SmartFlappy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class SmartFlappy : Agent
+{
+
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+    }
+
+
+    // public override void OnActionReceived(ActionBuffers actions)
+    // {
+    //     
+    //     
+    // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
