262a0e740f095326e4020b6631e6bacd2ed4fe20
jemca <46153814+jemca@users.noreply.github.com>
2021-03-31 01:18:48 -0400

    introduced a corridor changes scene a lot

--- Assets/Flappy Bird Style/ML-Agents/SmartFlappy.cs
+++ Assets/Flappy Bird Style/ML-Agents/SmartFlappy.cs
@@ -23,17 +23,24 @@ public class SmartFlappy : Agent
     public bool heuristicOnly;
 
     [Tooltip("Whether this is training mode or gameplay mode")]
-    public bool trainingMode;
-
+    // public bool trainingMode;
     public GameObject currentTarget;
+
     public GameObject currentEnemy;
+    public GameObject currentTunnel;
 
     public int currentTargetIndex = 0;
 
-    [FormerlySerializedAs("tagetBoxHeightFromCenter")]
     public float targetBoxHeightFromCenter;
 
     public float birdColumnGap;
+    public float tunnelBonus;
+
+
+    public Vector3 birdPosition;
+    public Vector3 tunnelPosition;
+    public Vector3 targetPosition;
+
 
     /// <summary>
     /// Initialize the agent
@@ -47,7 +54,7 @@ public class SmartFlappy : Agent
 
 
         // If not training mode, no max step, play forever
-        if (!trainingMode) MaxStep = 0;
+        // if (!trainingMode) MaxStep = 0;
     }
 
 
@@ -85,6 +92,7 @@ public class SmartFlappy : Agent
         currentTargetIndex = 0;
         currentEnemy = columnPool.columns[currentTargetIndex];
         currentTarget = columnPool.targets[currentTargetIndex];
+        currentTunnel = columnPool.tunnels[currentTargetIndex];
     }
 
 
@@ -99,15 +107,19 @@ public class SmartFlappy : Agent
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        var birdPosition = transform.localPosition;
-        var targetPosition = currentTarget.transform.localPosition;
+        birdPosition = transform.localPosition;
+        tunnelPosition = currentTunnel.transform.localPosition;
+        targetPosition = currentTarget.transform.localPosition;
+
         var velocity = rb2d.velocity.y;
         var enemyPosition = currentEnemy.transform.localPosition.x;
 
+
         sensor.AddObservation(birdPosition);
+        sensor.AddObservation(tunnelPosition);
 
         // Get a vector from the beak tip to the nearest flower
-        Vector3 toTargetDirection = targetPosition - birdPosition;
+        // Vector3 toTargetDirection = targetPosition - birdPosition;
 
         // sensor.AddObservation(toTargetDirection.normalized);
         sensor.AddObservation(targetPosition);
@@ -125,7 +137,7 @@ public class SmartFlappy : Agent
         // IT TAKES VALUES EVEN IF NOT TRAINING
         // Debug.Log(Time.time + " AI input: " + actions.DiscreteActions[0]);
 
-        if (trainingMode && isDead == false)
+        if (isDead == false)
         {
             //same thing
             // Debug.Log(this.StepCount);
@@ -136,31 +148,30 @@ public class SmartFlappy : Agent
             // life++;
             // var bonus = 0.01f * life;
             // AddReward(bonus);
-            
+
             //NEW STAY ALIVE AND STAY ALIGNED BONUS
-            var birdX = transform.position.x;
-            var enemyX = currentEnemy.transform.position.x;
+            var birdX = transform.localPosition.x;
+            var enemyX = currentEnemy.transform.localPosition.x;
             var gap = enemyX - birdX;
 
             if (gap > birdColumnGap)
             {
-                var birdY = transform.position.y;
-                var targetY = currentTarget.transform.position.y;
+                var birdY = transform.localPosition.y;
+                var targetY = currentTarget.transform.localPosition.y;
                 var top = targetY + targetBoxHeightFromCenter;
                 var bottom = targetY - targetBoxHeightFromCenter;
 
                 if (birdY < top && birdY > bottom)
                 {
                     // Debug.Log($"birdy{birdY} top{top} bottom{bottom}  ");
-                    var bonus = 0.01f * StepCount;
+                    var bonus = 1f; //* StepCount;
                     AddReward(bonus);
                     life = StepCount;
-
                 }
             }
 
 
-            if (actions.DiscreteActions[0] == 0)
+            if (actions.DiscreteActions[0] == 1)
             {
                 // Debug.Log("agent received input to fly");
 
@@ -187,7 +198,7 @@ public class SmartFlappy : Agent
     private void Update()
     {
         //Don't allow control if the bird has died.
-        if (isDead == false && heuristicOnly)
+        if (heuristicOnly && isDead == false)
         {
             //Look for input to trigger a "flap".
             if (Input.GetMouseButtonDown(0) == true)
@@ -227,6 +238,9 @@ public class SmartFlappy : Agent
         rb2d.velocity = Vector2.zero;
         // If the bird collides with something set it to dead...
         isDead = true;
+
+        // tunnelBonus = 0;
+        
         //...tell the Animator about it...
         anim.SetTrigger("Die");
         //...and tell the game control about it.
@@ -245,28 +259,50 @@ public class SmartFlappy : Agent
         if (other.gameObject.CompareTag("SCORE"))
         {
             // Time.timeScale = 0;
-
+    
             //If the bird hits the trigger collider in between the columns then
             //tell the game control that the bird scored.
-
+    
             //NEW
             gameControl.BirdScored();
             //OLD
             // GameControl.instance.BirdScored();
-
+    
             var targetReward = 1000 + 200 * passedCol;
             passedCol++;
-
+    
             AddReward(targetReward);
-
+    
             if (passedCol > 100) Debug.Log(Time.time + "PASSED COLUMN:" + passedCol + " " + targetReward);
-
+    
             currentTargetIndex = (currentTargetIndex + 1) % columnPool.columnPoolSize;
-
+    
             // Debug.Log("current target index:" + currentTargetIndex);
-
+    
             currentEnemy = columnPool.columns[currentTargetIndex];
             currentTarget = columnPool.targets[currentTargetIndex];
         }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("TUNNEL"))
+        {
+            tunnelBonus += 0.1f; //* StepCount;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("TUNNEL"))
+        {
+            
+            AddReward(tunnelBonus);
+            // Debug.Log(tunnelBonus);
+
+            tunnelBonus = 0;
+        }
+
     }
 }
--- Assets/Flappy Bird Style/Scripts/ColumnPool.cs
+++ Assets/Flappy Bird Style/Scripts/ColumnPool.cs
@@ -6,65 +6,61 @@ using Random = UnityEngine.Random;
 
 public class ColumnPool : MonoBehaviour
 {
-    // public SmartFlappy bird;
-    // public GameObject columnPrefab; //The column game object.
-    // public GameObject targetPrefab; //SCORE POINTS
-
     public GameControl gameControl;
 
-    public float spawnRate = 3f; //How quickly columns spawn.
-    public float columnMin = -1f; //Minimum y value of the column position.
-    public float columnMax = 3.5f; //Maximum y value of the column position.
 
-    public int currentColumn = 0; //Index of the current column in the collection.
+//OLD VARIABLES
+    // public Vector2 objectPoolPosition = new Vector2(-15, -25); //A holding position for our unused columns offscreen.
+    // public float spawnRate = 3f; //How quickly columns spawn.
+    // public float columnMin = -1f; //Minimum y value of the column position.
+    // public float columnMax = 3.5f; //Maximum y value of the column position.
+    // private float timeSinceLastSpawned;
 
-    public Vector2 objectPoolPosition = new Vector2(-15, -25); //A holding position for our unused columns offscreen.
 
-    
-    
-    
-    
     public float spawnXPosition = 10f;
 
-    private float timeSinceLastSpawned;
 
-    [Space(20)] public int columnPoolSize = 4; //How many columns to keep on standby.
+    [Space(20)] public int currentColumn = 0; //Index of the current column in the collection.
+
+    public int columnPoolSize = 4; //How many columns to keep on standby.
     public GameObject[] columns; //Collection of pooled columns.
     public GameObject[] targets; //Collection of pooled columns.
+    public GameObject[] tunnels; //Collection of pooled columns.
 
-
-    public float[] initialPositions;
+    public float[] initialXPositions;
+    public float[] initialYPositions;
+    public bool randomize;
     public float targetOffsetFromColumn = 0.5f;
 
-    void Start()
-    {
-        timeSinceLastSpawned = 0f;
-
-        // initialPositions = new float[columnPoolSize];
-        // for (int i = 0; i < columnPoolSize; i++)
-        // {
-        //     initialPositions[i] = columns[i].transform.position.x;
-        // }
-
-
-        // // CREATED IN THE SCENE NOT NEEDED ANYMORE
-        // //Initialize the columns collection.
-        // columns = new GameObject[columnPoolSize];
-        // targets = new GameObject[columnPoolSize];
-        // //Loop through the collection... 
-        // for (int i = 0; i < columnPoolSize; i++)
-        // {
-        //     //...and create the individual columns.
-        //     columns[i] = Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity, transform.parent);
-        //     columns[i].GetComponent<ScrollingObject>().gameControl = gameControl;
-        //     
-        //     targets[i] = Instantiate(targetPrefab, objectPoolPosition, Quaternion.identity, transform.parent);
-        //     targets[i].GetComponent<ScrollingObject>().gameControl = gameControl;
-        //     
-        //     
-        //     
-        // }
-    }
+    // void Start()
+    // {
+    //     // timeSinceLastSpawned = 0f;
+    //
+    //     // initialPositions = new float[columnPoolSize];
+    //     // for (int i = 0; i < columnPoolSize; i++)
+    //     // {
+    //     //     initialPositions[i] = columns[i].transform.position.x;
+    //     // }
+    //
+    //
+    //     // // CREATED IN THE SCENE NOT NEEDED ANYMORE
+    //     // //Initialize the columns collection.
+    //     // columns = new GameObject[columnPoolSize];
+    //     // targets = new GameObject[columnPoolSize];
+    //     // //Loop through the collection... 
+    //     // for (int i = 0; i < columnPoolSize; i++)
+    //     // {
+    //     //     //...and create the individual columns.
+    //     //     columns[i] = Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity, transform.parent);
+    //     //     columns[i].GetComponent<ScrollingObject>().gameControl = gameControl;
+    //     //     
+    //     //     targets[i] = Instantiate(targetPrefab, objectPoolPosition, Quaternion.identity, transform.parent);
+    //     //     targets[i].GetComponent<ScrollingObject>().gameControl = gameControl;
+    //     //     
+    //     //     
+    //     //     
+    //     // }
+    // }
 
 
     //This spawns columns as long as the game is not over.
@@ -109,11 +105,15 @@ public class ColumnPool : MonoBehaviour
     {
         if (gameControl.gameOver == false && columns[currentColumn].transform.position.x < -10)
         {
-            float spawnYPosition = Random.Range(columnMin, columnMax);
+            // float spawnYPosition = randomize ? Random.Range(columnMin, columnMax) : initialYPositions[currentColumn];
+            float spawnYPosition = initialYPositions[currentColumn];
+
 
             //...then set the current column to that position.
             columns[currentColumn].transform.localPosition = new Vector3(spawnXPosition, spawnYPosition, 0);
-            targets[currentColumn].transform.localPosition = new Vector3(spawnXPosition + targetOffsetFromColumn, spawnYPosition, 0);
+            targets[currentColumn].transform.localPosition =
+                new Vector3(spawnXPosition + targetOffsetFromColumn, spawnYPosition, 0);
+            tunnels[currentColumn].transform.localPosition = new Vector3(spawnXPosition, spawnYPosition, 0);
 
 
             currentColumn = (currentColumn + 1) % columnPoolSize;
@@ -122,32 +122,29 @@ public class ColumnPool : MonoBehaviour
 
     public void ResetColumns()
     {
-        
         for (int i = 0; i < columnPoolSize; i++)
         {
-            float spawnYPosition = Random.Range(columnMin, columnMax);
+            // float spawnYPosition = randomize ? Random.Range(columnMin, columnMax) : initialYPositions[i];
 
-            columns[i].transform.localPosition = new Vector3(initialPositions[i], spawnYPosition, 0);
-            targets[i].transform.localPosition = new Vector3(initialPositions[i] + targetOffsetFromColumn, spawnYPosition, 0);
-    
+            float spawnYPosition = initialYPositions[i];
+            // Debug.Log(initialYPositions[i]);
+
+
+            columns[i].transform.localPosition = new Vector3(initialXPositions[i], spawnYPosition, 0);
+            targets[i].transform.localPosition = new Vector3(initialXPositions[i] + targetOffsetFromColumn, spawnYPosition, 0);
+            tunnels[i].transform.localPosition = new Vector3(initialXPositions[i], spawnYPosition, 0);
+
             currentColumn = 0;
         }
-        
-        
-        
-        
-        
-        
-        
-    //  old one   
-    // for (int i = 0; i < columnPoolSize; i++)
-    // {
-    //     columns[i].transform.localPosition = objectPoolPosition;
-    //     targets[i].transform.localPosition = objectPoolPosition;
-    //
-    //     currentColumn = 0;
-    // }
+
+
+        //  old one   
+        // for (int i = 0; i < columnPoolSize; i++)
+        // {
+        //     columns[i].transform.localPosition = objectPoolPosition;
+        //     targets[i].transform.localPosition = objectPoolPosition;
+        //
+        //     currentColumn = 0;
+        // }
     }
-    
-    
 }
